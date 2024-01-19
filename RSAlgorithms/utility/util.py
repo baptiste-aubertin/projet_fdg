221f02c4d9c04469a5318803c99557bab81f6599
baptiste-aubertin <baptiste.aubertin@etu.umontpellier.fr>
2024-01-19 18:13:02 +0100

    code refactored

---
+++ RSAlgorithms/utility/util.py
@@ -0,0 +1,15 @@
+# encoding:utf-8
+
+import pickle
+
+
+def save_data(obj, filename):
+    pickle.dump(obj, open(filename, 'wb'))
+    pass
+
+
+def load_data(filename):
+    f = open(filename, 'rb')
+    model = pickle.load(f)
+    print(filename + ' load data model finished.')
+    return model
