2d73c28ae843b491c36d44ec660135500d23c2d4
baptiste-aubertin <baptiste.aubertin@univ-tlse3.fr>
2024-01-22 01:42:12 +0100

    epinion datas

--- RSAlgorithms/configx/config_ep.py
+++ RSAlgorithms/configx/config_ep.py
@@ -1,5 +1,5 @@
 # encoding:utf-8
-class ConfigX():
+class ConfigEp():
     """
     docstring for ConfigX
 
--- RSAlgorithms/utility/cross_validation.py
+++ RSAlgorithms/utility/cross_validation.py
@@ -12,22 +12,45 @@ Split ratings into five folds
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 
+
 def split_5_folds(configx):
     K = configx.k_fold_num
     names = ['user_id', 'item_id', 'rating']
     if not os.path.isfile(configx.rating_path):
-        print("the format of rating data is wrong")
+        print("there is no such file:%s" % configx.rating_path)
         sys.exit()
+    
     df = pd.read_csv(configx.rating_path, sep=configx.sep, names=names)
+    
+    # Ensure the user_id and item_id are integers
+    df.user_id = df.user_id.astype(int)
+    df.item_id = df.item_id.astype(int)
+
+    # Remapping user_id and item_id
+    user_ids = df['user_id'].unique()
+    item_ids = df['item_id'].unique()
+    user_id_map = {old: new for new, old in enumerate(user_ids)}
+    item_id_map = {old: new for new, old in enumerate(item_ids)}
+    df['user_id'] = df['user_id'].map(user_id_map)
+    df['item_id'] = df['item_id'].map(item_id_map)
+
+    users = df.user_id.unique()
+
     ratings = coo_matrix((df.rating, (df.user_id, df.item_id)))
-    users = np.unique(ratings.row)
-    ratings = ratings.tocsr()
+    
+    # Convert to csr format
+    try:
+        ratings = ratings.tocsr()
+    except Exception as e:
+        print(f"Error converting to CSR format: {e}")
+        return
+
 
     rows = list()
     cols = list()
     vals = list()
     nonzeros = list()
-
+    print('D1')
     for k in range(K):
         size_of_bucket = int(ratings.nnz / K)
         if k == K - 1:
@@ -36,7 +59,7 @@ def split_5_folds(configx):
         cols.append(np.zeros(size_of_bucket))
         vals.append(np.zeros(size_of_bucket))
         nonzeros.append(0)
-
+    print('D2')
     for i, user in enumerate(users):
         items = ratings[user, :].indices
         rating_vals = ratings[user, :].data
@@ -59,11 +82,11 @@ def split_5_folds(configx):
             cols[k][from_ind:to_ind] = items[k_index_list]
             vals[k][from_ind:to_ind] = rating_vals[k_index_list]
             nonzeros[k] += sum(k_index_list)
-
+    print('D3')
     if not os.path.exists(configx.rating_cv_path):
         os.makedirs(configx.rating_cv_path)
         print(f'{configx.rating_cv_path} folder has been established.')
-
+    print('D4')
     for k, (row, col, val, nonzero) in enumerate(zip(rows, cols, vals, nonzeros)):
         bucket_df = pd.DataFrame({'user': row[:nonzero], 'item': col[:nonzero], 'rating': val[:nonzero]},
                                  columns=['user', 'item', 'rating'])
